6ae99b4ece2b02dd96973e382d96285875a414fc
Sven Hecht <sven.hecht@logmein.com>
2017-05-19 11:41:35 +0200

    Fix rollup config for package

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,10 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
 
 export default {
   entry: 'index.js',
-  format: 'umd',
-  exports: 'named',
   dest: 'dist/bundle.js',
-  moduleName: 'intent',
   plugins: [
     resolve(),
     babel({
@@ -14,5 +12,17 @@ export default {
       presets: ['es2015-rollup', 'es2016', 'es2017', 'stage-0']
     })
   ],
-  sourceMap: true
+  targets: [
+    {
+      dest: pkg.main,
+      format: 'umd',
+      moduleName: 'intent.js',
+      sourceMap: true
+    },
+    {
+      dest: pkg.module,
+      format: 'es',
+      sourceMap: true
+    }
+  ]
 };
